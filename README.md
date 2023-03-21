488feeef370a6a8f4b719d5dc5000c4695347529
Gadko <megotim@mail.ru>
2023-03-21 15:33:28 +0300

    README

--- README.md
+++ README.md
@@ -9,5 +9,8 @@
 
 Интерактивная страница, куда можно добавлять фотографии, удалять их и ставить лайки.
 
+**Ссылки**
+
 * [Ссылка на GitHub проекта](https://gadko.github.io/mesto-project/)
 
+Анжелика Сидовичева - https://github.com/AnzhelikaLanina/mesto-project
--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,24 +1,23 @@
-
 export default class UserInfo {
-    constructor({ profileName, profileDescription, profileAvatar }){
-        this._profileName = document.querySelector(profileName);
-        this._profileAbout = document.querySelector(profileDescription);
-        this._profileAvatar = document.querySelector(profileAvatar);
-    }
-    // объект с данными пользователя
-    getUserInfo(){
-        return {
-            id: this._id,
-            name: this._profileName.textContent,
-            about: this._profileAbout.textContent,
-            avatar: this._profileAvatar.style.backgroundImage
-        };
-    }
-    // новые данные пользователя
-    setUserInfo({name,about,avatar,_id}){
-        this._profileName.textContent = name;
-        this._profileAbout.textContent = about;
-        this._profileAvatar.style.backgroundImage = `url(${avatar})`;
-        this.userId = _id;
-    }
+  constructor({ profileName, profileDescription, profileAvatar }){
+      this._profileName = document.querySelector(profileName);
+      this._profileAbout = document.querySelector(profileDescription);
+      this._profileAvatar = document.querySelector(profileAvatar);
+  }
+  // объект с данными пользователя
+  getUserInfo(){
+      return {
+          id: this._id,
+          name: this._profileName.textContent,
+          about: this._profileAbout.textContent,
+          avatar: this._profileAvatar.style.backgroundImage
+      };
+  }
+  // новые данные пользователя
+  setUserInfo({name,about,avatar,_id}){
+      this._profileName.textContent = name;
+      this._profileAbout.textContent = about;
+      this._profileAvatar.style.backgroundImage = `url(${avatar})`;
+      this.userId = _id;
+  }
 }
